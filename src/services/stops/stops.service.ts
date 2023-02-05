af9d6b1d8daba870b36afa9c7740cbb696b2a429
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2023-02-05 19:30:32 -0300

    patch: errorHandling refactor on service

--- src/services/stops/stops.service.ts
+++ src/services/stops/stops.service.ts
@@ -1,4 +1,5 @@
 import { StopsApiResponse } from "../../interfaces/Stop.ts"
+import { createError, ErrorTypes } from "./errorGenerator.ts"
 
 const RUTA_LINEA = 'http://www.larioja.miredbus.com.ar/rest/rutaLinea/'
 
@@ -6,20 +7,13 @@ export async function getStops(linea: string): Promise<StopsApiResponse>{
     try {
         // deno-lint-ignore no-explicit-any
         const res: any = await fetch(`${RUTA_LINEA}${linea}`)
-        const data: StopsApiResponse = await <StopsApiResponse> res.json()
+        const data: StopsApiResponse = await <StopsApiResponse>res.json()
         if(!data.nodos.length){
-            const e = new Error()
-            Object.assign(e, {
-                status: 404,
-                body: {
-                    message: 'Línea invalida o sin paradas',
-                    apiResponse: data
-                }
-            })
-            throw e
+            throw createError(ErrorTypes.NotFound, data)
         }
         return data
     } catch (error) {
         throw error
     }
 }
+
